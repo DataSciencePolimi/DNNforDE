c8034010860e8793c090a9ed8bbfc0a9c043f331
Marco Di Giovanni <marco@MacBook-Pro-di-Marco.local>
2018-05-29 13:11:54 +0200

    Add general framework for ODE and PDE, with some problems already defined

--- functions1d.py
+++ functions1d.py
@@ -1,13 +1,34 @@
 import torch
 import numpy as np
 
+l = 3
+def real0(x):
+    return torch.exp(-l*x)
+
+def trial0(f, x):
+    return 1 + x * f(x)
+
+def diff0(t, x, gx):
+    return gx[0] + l * x
+
+int0=(0, 1)
+
+degree0 = 1
+
 def real1(x):
     return torch.exp(-x**2/2)/(1+x+x**3)+x**2
 
 def trial1(f, x):
     return 1+x*f(x)
 
+def diff1(t, x, gx):
+    t2 = t**2
+    t3 = t**3
+    return gx[0] + (t + (1+3*t2)/(1+t+t3))*x - t3 - 2*t - t2*(1+3*t2)/(1+t+t3)
+
 int1=(0, 1)
+
+degree1 = 1
 
 def real2(x):
     return torch.exp(-x/5)*torch.sin(x)
@@ -15,7 +36,12 @@ def real2(x):
 def trial2(f, x):
     return x*f(x)
 
+def diff2(t, x, gx):
+    return gx[0] + x/5 - torch.exp(-t/5) * torch.cos(t)
+
 int2=(0, 2)
+
+degree2 = 1
 
 def real3(x):
     return torch.exp(-x/5)*torch.sin(x)
@@ -23,10 +49,12 @@ def real3(x):
 def trial3(f, x):
     return x+x**2*f(x)
 
+def diff3(t, x, gx):
+    return gx[1] + gx[0]/5 + x + torch.exp(-t/5) * torch.cos(t)/5
+
 int3=(0, 2)
 
-def real3b(x):
-    return torch.exp(-x/5)*torch.sin(x)
+degree3 = 2
 
 def trial3b(f, x):
     return x*np.sin(1.)*np.exp(-1./5)+x*(1-x)*f(x)
@@ -34,23 +62,53 @@ def trial3b(f, x):
 int3b=(0, 1)
 
 function={
+'p0':real0,
 'p1':real1,
 'p2':real2,
 'p3':real3,
-'p3b':real3b
+'p3b':real3
 }
 
 trial={
+'p0':trial0,
 'p1':trial1,
 'p2':trial2,
 'p3':trial3,
 'p3b':trial3b
 }
 
+diff_eq={
+'p0':diff0,
+'p1':diff1,
+'p2':diff2,
+'p3':diff3,
+'p3b':diff3
+}
+
 interval={
+'p0':int0,
 'p1':int1,
 'p2':int2,
 'p3':int3,
 'p3b':int3b
 }
+
+degree={
+'p0':degree0,
+'p1':degree1,
+'p2':degree2,
+'p3':degree3,
+'p3b':degree3
+}
+
+
+
+
+
+
+
+
+
+
+
 
--- functions2d.py
+++ functions2d.py
@@ -6,9 +6,10 @@ def real5(x):
     x2 = x[:, 1]
     return torch.exp(-x1)*(x1+x2**3)
 
-def trial5(f, x):
-    x1 = x[:, 0]
-    x2 = x[:, 1]
+def trial5(f, x1, x2):
+    #x1 = x[:, 0]
+    #x2 = x[:, 1]
+    x = torch.cat((x1.view(-1, 1), x2.view(-1, 1)), 1)
     x2_3 = x2**3
     e1 = np.exp(-1.)
     ex1 = torch.exp(-x1)
@@ -16,16 +17,23 @@ def trial5(f, x):
     A = (1-x1)*x2_3+x1*(1+x2_3)*e1+(1-x2)*x1*(ex1-e1)+x2*((1+x1)*ex1-(1-x1+2*x1*e1))
     return A + x1*(1-x1)*x2*(1-x2)*f(x).t()
 
+def diff5(t, x, gx):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    delta2x = gx[1][0] + gx[1][1]
+    return delta2x - torch.exp(-t0)*(t0 - 2 + t1**3 + 6 * t1)
+
 int5=(0, 1, 0, 1)
+
+degree5 = 2
 
 def real6(x, a=3):
     x1 = x[:, 0]
     x2 = x[:, 1]
     return torch.exp(-(a*x1+x2)/5)*torch.sin((a*x1)**2+x2)
 
-def trial6(f, x, a=3):
-    x1 = x[:, 0]
-    x2 = x[:, 1]
+def trial6(f, x1, x2, a=3):
+    x = torch.cat((x1.view(-1, 1), x2.view(-1, 1)), 1)
     a2 = a**2
     x1_2 = x1**2
     eax5 = torch.exp(-a*x1/5)
@@ -40,7 +48,17 @@ def trial6(f, x, a=3):
     A = (1-x1)*c1+x1*c2+(1-x2)*c3+x2*c4
     return A + x1*(1-x1)*x2*(1-x2)*f(x).t()
 
+def diff6(t, x, gx, a=3):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    a2 = a**2
+    delta2x = gx[1][0] + gx[1][1]
+    angle = (a*t0)**2+t1
+    return delta2x - torch.exp(-(a*t0 + t1)/5)*((-4*a**3*t0/5-2/5+2*a2)*torch.cos(angle)+(1/25-1-4*(a2*t0)**2+a2/25)*torch.sin(angle))
+
 int6=(0, 1, 0, 1)
+
+degree6 = 2
 
 # not complete
 
@@ -49,23 +67,39 @@ def real7(x):
     x2 = x[:, 1]
     return x2**2*torch.sin(x1*np.pi)
 
-def trial7(f, x):
-    x1 = x[:, 0]
-    x2 = x[:, 1]
+def trial7(f, x1, x2):
+    x = torch.cat((x1.view(-1, 1), x2.view(-1, 1)), 1)
     x_1 = Variable(torch.cat((torch.Tensor(x1), torch.Tensor(np.ones(len(x1)))), 1))
     print(x_1)
     B = 1
     return B + x1*(1-x1)*x2*(f(x) - f(x_1) - 1)
 
+def diff7(t, x, gx):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    delta2x = gx[1][0] + gx[1][1]
+    return delta2x - (2 - (np.pi * t1)**2) * torch.sin(np.pi * t0)
+
 int7=(0, 1, 0, 1)
+
+degree7 = 2
 
 def real8(x):
     return torch.exp(-x/5)*torch.sin(x)
 
-def trial8(f, x):
+def trial8(f, x1, x2):
+    x = torch.cat((x1.view(-1, 1), x2.view(-1, 1)), 1)
     return x*np.sin(1.)*np.exp(-1./5)+x*(1-x)*f(x)
 
+def diff8(t, x, gx):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    delta2x = gx[1][0] + gx[1][1]
+    return delta2x + x * gx[0][1] - torch.sin(np.pi * t0) * (2 - (np.pi * t1)**2 + 2 * t1**3 * torch.sin(np.pi * t0))
+
 int8=(0, 1, 0, 1)
+
+degree8 = 2
 
 function={
 'p5':real5,
@@ -88,3 +122,16 @@ interval={
 'p8':int8
 }
 
+diff_eq={
+'p5':diff5,
+'p6':diff6,
+'p7':diff7,
+'p8':diff8
+}
+
+degree={
+'p5':degree5,
+'p6':degree6,
+'p7':degree7,
+'p8':degree8
+}
