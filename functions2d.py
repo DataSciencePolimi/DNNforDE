a9c13c5375ce6d08ce288796eb3570c0b262f48a
Marco Di Giovanni <marco@MacBook-Pro-di-Marco.local>
2018-06-01 18:57:49 +0200

    Add new problems

--- functions2d.py
+++ functions2d.py
@@ -101,37 +101,102 @@ int8=(0, 1, 0, 1)
 
 degree8 = 2
 
+def burger(t, x, gx):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    c = 0.01/np.pi
+    return gx[0][0] + x * gx[0][1] - c * gx[1][1]
+
+def real9(t):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    return 2*t1/(1+2*t0)
+
+def trial9(f, x1, x2):
+    x = torch.cat((x1.view(-1, 1), x2.view(-1, 1)), 1)
+    return 2*x2 + x1*f(x).t()
+
+degree9 = 2
+
+int9 = (0, 1, -1, 1)
+
+def real10(t, ni= 0.01/np.pi):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    frac = t1/t0
+    return frac - np.pi * torch.tanh(np.pi*0.5*frac/ni)/t0
+
+def trial10(f, x1, x2, ni= 0.01/np.pi):
+    x = torch.cat((x1.view(-1, 1), x2.view(-1, 1)), 1)
+    return (1./x1 - (np.pi/x1)**2 *0.5 / ni) * x2 + x2**2 * f(x).t()
+
+int10 = (0.1, 1.1, 0, 1)
+
+
+def laplace(t, x, gx):
+    return gx[1][0]+gx[1][1]
+
+def real_laplace(t):
+    t0 = t[:, 0]
+    t1 = t[:, 1]
+    pi = np.pi
+    return torch.sin(pi*t0) * (torch.exp(pi*t1)-torch.exp(-pi*t1)) / (np.exp(pi)-np.exp(-pi))
+
+def trial_laplace(f, x1, x2):
+    x = torch.cat((x1.view(-1, 1), x2.view(-1, 1)), 1)
+    return x2*torch.sin(np.pi*x1) + x1 * (1-x1) * x2 * (1-x2) * f(x).t()
+
+int_laplace = (0, 1, 0, 1)
+
+degree_laplace = 2
+
+
 function={
 'p5':real5,
 'p6':real6,
 'p7':real7,
-'p8':real8
+'p8':real8,
+'burger':real9,
+'burger2':real10,
+'laplace':real_laplace
 }
 
 trial={
 'p5':trial5,
 'p6':trial6,
 'p7':trial7,
-'p8':trial8
+'p8':trial8,
+'burger':trial9,
+'burger2':trial10,
+'laplace':trial_laplace
 }
 
 interval={
 'p5':int5,
 'p6':int6,
 'p7':int7,
-'p8':int8
+'p8':int8,
+'burger':int9,
+'burger2':int10,
+'laplace':int_laplace
 }
 
 diff_eq={
 'p5':diff5,
 'p6':diff6,
 'p7':diff7,
-'p8':diff8
+'p8':diff8,
+'burger':burger,
+'burger2':burger,
+'laplace':laplace
 }
 
 degree={
 'p5':degree5,
 'p6':degree6,
 'p7':degree7,
-'p8':degree8
+'p8':degree8,
+'burger':degree9,
+'burger2':degree9,
+'laplace':degree_laplace
 }
